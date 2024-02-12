1b8166449369f0dc39a85e544d7af2797fa38800
Anupam-Roy16 <123891596+Anupam-Roy16@users.noreply.github.com>
2024-02-12 15:35:09 +0600

    Time: 59 ms (73.26%), Space: 17.8 MB (67.91%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,5 +1,8 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
+        'sapce o(n), time o(n),  '
+        'it can be solved by two poointer first sort then search '
+        
         dictionary = {}
         for i in range(len(nums)):
             if target-nums[i] in dictionary:
