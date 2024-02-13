d9dac6fcc4fd9f6c815e889ec7b64dc850bf22fa
Anupam-Roy16 <123891596+Anupam-Roy16@users.noreply.github.com>
2024-02-13 18:32:22 +0600

    Time: 737 ms (53.08%), Space: 20.6 MB (81.72%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -1,33 +1,34 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        v=[]
+        v = []
         nums.sort()
         i = 0 
         while i < len(nums)-2:
-            left,right=i+1,len(nums)-1
-            print(i,"we")
-            while left<right:
-                sum=nums[i]+nums[left]+nums[right]
-                #print(left,right)
-                if sum==0:
+            left ,right = i+1, len(nums)-1
+            
+            while left < right:
+                
+                sum = nums[i]+nums[left]+nums[right]
+                if sum == 0:
+                    
                     l=[nums[i],nums[left],nums[right]]
-                    #print(l)
                     v.append(l)
+                    'find diffrent element'
                     while (left+1)<len(nums) and  nums[left] == nums[left+1]:
+                    
                         left += 1
                     left += 1
+                    'find diffrent element'
                     while right-1>=0 and nums[right] == nums[right-1]:
                         right -= 1
                     right -= 1
-                elif sum<0:
-                    left=left+1
+                elif sum < 0:
+                    left = left+1
                 else:
-                    right=right-1
-                #print(left,right)
-            while i+1<len(nums) and nums[i] == nums[i+1]:
+                    right = right-1
+            'find diffrent element'
+            while i+1 < len(nums) and nums[i] == nums[i+1]:
                 i +=1
             i += 1
-            
         return v
-        #return list(v)
         
