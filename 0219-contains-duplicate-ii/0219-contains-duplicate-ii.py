e37a3f2c6450513c98effec178f8f68f1cb24572
Anupam-Roy16 <123891596+Anupam-Roy16@users.noreply.github.com>
2024-05-03 17:37:40 +0600

    Time: 463 ms (53.44%), Space: 30 MB (20.30%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.py
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.py
@@ -1,4 +1,5 @@
 class Solution:
+    #first duplicate number and its index saved in hash then iterate over list and if duplicate number index difference is less or equal then k then true otherwise False  
     def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
         hash_table = {}
         for i in range(len(nums)):
