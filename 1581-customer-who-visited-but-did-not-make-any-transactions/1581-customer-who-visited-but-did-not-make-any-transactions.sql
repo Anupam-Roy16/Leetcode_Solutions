493105c15189503c1e5ae5aba568788ed636d2d5
Anupam Roy <123891596+Anupam-Roy16@users.noreply.github.com>
2024-06-30 17:22:57 +0600

    Time: 1154 ms (89.42%), Space: 0B (100.00%) - LeetHub

--- 1581-customer-who-visited-but-did-not-make-any-transactions/1581-customer-who-visited-but-did-not-make-any-transactions.sql
+++ 1581-customer-who-visited-but-did-not-make-any-transactions/1581-customer-who-visited-but-did-not-make-any-transactions.sql
@@ -4,7 +4,13 @@
 
 
 
+# select customer_id,count(customer_id) as count_no_trans
+# from Visits left join Transactions on Visits.visit_id = Transactions.visit_id
+# where amount IS NULL AND transaction_id IS NULL
+# group by customer_id
+
+
 select customer_id,count(customer_id) as count_no_trans
 from Visits left join Transactions on Visits.visit_id = Transactions.visit_id
-where amount IS NULL AND transaction_id IS NULL
+where Transactions.visit_id  IS NULL
 group by customer_id
