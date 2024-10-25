7f5cb6770366ebeea48d26eabb7a65b9a2d7bdf7
Anupam Roy <123891596+Anupam-Roy16@users.noreply.github.com>
2024-10-25 20:30:01 +0600

    Time: 3 ms (98.57%), Space: 18.1 MB (10.16%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.py
+++ 0229-majority-element-ii/0229-majority-element-ii.py
@@ -1,15 +1,26 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> List[int]:
-        dic = {}
-        n = len(nums)//3
-        ans = []
-        s = set()
-        for x in nums:
-            if x not in dic:
-                dic[x] =1
+        candidate1, candidate2 = None, None
+        count1, count2 = 0, 0
+
+        for num in nums:
+            if candidate1 == num:
+                count1 += 1
+            elif candidate2 == num:
+                count2 += 1
+            elif count1 == 0:
+                candidate1, count1 = num, 1
+            elif count2 == 0:
+                candidate2, count2 = num, 1
             else:
-                dic[x] +=1
-            if dic[x] > n:
-                s.add(x)
-        return list(s)
-        
+                count1 -= 1
+                count2 -= 1
+
+        # Step 2: Verify candidates
+        result = []
+        if nums.count(candidate1) > len(nums) // 3:
+            result.append(candidate1)
+        if candidate2 is not None and nums.count(candidate2) > len(nums) // 3:
+            result.append(candidate2)
+
+        return result
