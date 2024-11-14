04c7460791536fdd047c5f19772e55ac723067bb
Anupam Roy <123891596+Anupam-Roy16@users.noreply.github.com>
2024-11-14 09:20:08 +0600

    Time: 2 ms (75.23%), Space: 17.4 MB (54.09%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
@@ -1,18 +1,27 @@
 class Solution:
+    def make_row_zero(self,matrix,i):
+        for j in range(len(matrix[0])):
+            if matrix[i][j] !=0:
+                matrix[i][j] = 2**31
+    def make_col_zero(self,matrix,j):
+        for i in range(len(matrix)):
+            if matrix[i][j] !=0:
+                matrix[i][j] = 2**31
+
     def setZeroes(self, matrix: List[List[int]]) -> None:
         """
         Do not return anything, modify matrix in-place instead.
         """
-        location = []
         for i in range(len(matrix)):
             for j in range(len(matrix[0])):
-                if matrix[i][j] == 0:
-                    location.append([i,j])
-        for x in location:
-            for i in range(len(matrix[0])):
-                matrix[x[0]][i] = 0
-            for i in range(len(matrix)):
-                matrix[i][x[1]] = 0
+                if matrix[i][j]==0:
+                    self.make_row_zero(matrix,i)
+                    self.make_col_zero(matrix,j)
+        
+        for i in range(len(matrix)):
+            for j in range(len(matrix[0])):
+                if matrix[i][j] == 2**31:
+                    matrix[i][j]=0
         
                 
                 
