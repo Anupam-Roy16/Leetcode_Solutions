f21ee600ea735c626da31db2be0259f59476fbb4
Anupam Roy <123891596+Anupam-Roy16@users.noreply.github.com>
2025-08-01 14:17:28 +0600

    Time: 43 ms (86.79%), Space: 17.7 MB (98.69%) - LeetHub

---
+++ 2816-lexicographically-smallest-palindrome/2816-lexicographically-smallest-palindrome.py
@@ -0,0 +1,14 @@
+class Solution:
+    def makeSmallestPalindrome(self, s: str) -> str:
+        left , right = 0, len(s)-1
+        s = list(s)
+        while left < right:
+            if s[left] !=s[right]:
+                if ord(s[left]) < ord(s[right]):
+                    s[right] = s[left]
+                else:
+                    s[left] = s[right]
+            left +=1
+            right -=1
+        return "".join(s)
+        
