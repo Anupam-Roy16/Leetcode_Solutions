d96c57e750bcba8431b0c18b0a65a6c5605232e2
Anupam Roy <123891596+Anupam-Roy16@users.noreply.github.com>
2025-08-02 18:05:41 +0600

    Time: 1 ms (57.64%), Space: 17.9 MB (44.79%) - LeetHub

--- 3484-lexicographically-smallest-string-after-a-swap/3484-lexicographically-smallest-string-after-a-swap.py
+++ 3484-lexicographically-smallest-string-after-a-swap/3484-lexicographically-smallest-string-after-a-swap.py
@@ -8,7 +8,7 @@ class Solution:
                     s[i] = s[i+1]
                     s[i+1] = tmp
                     return "".join(s)
-                    break
+                    
         return s
 
         
