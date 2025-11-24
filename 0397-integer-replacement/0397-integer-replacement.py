6e984240083165466e1110824bf2c38fa91ebba5
Anupam Roy <123891596+Anupam-Roy16@users.noreply.github.com>
2025-11-24 22:00:20 +0600

    Time: 99 ms (10.22%), Space: 17.7 MB (92.14%) - LeetHub

---
+++ 0397-integer-replacement/0397-integer-replacement.py
@@ -0,0 +1,18 @@
+class Solution:
+    def integerReplacement(self, n: int) -> int:
+        map = {}
+        def fun(n):
+            if n == 1:
+                return 0
+            elif n%2==0:
+                return 1 + fun(n//2) 
+            else:
+                return 1+ min(fun(n+1),fun(n-1))
+        return fun(n)
+            
+
+
+
+
+
+        
